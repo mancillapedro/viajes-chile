98e2508d471dc246e60ef9a6840ef23da1e32df7
_ <pedropmancillac@gmail.com>
2022-03-31 17:32:24 -0300

    avance funcionalidad presentacion

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,18 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+const collapse = () => {
+    if ($(document).width() > 768){$('.parrafos').addClass('show'); }
+    else{ $('.parrafos').removeClass('show'); }
+}
 
 $(function () {
 
     $(document).scroll(function(){
         $('nav').attr('style', `background-color: rgba(0,0,0,${window.scrollY/window.innerHeight})`);
     });
+
+    collapse()
+    $(window).resize(() => collapse())
 
 });
