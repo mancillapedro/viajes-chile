7f981ee47006348d4a9a3524bf8318e83d23b588
_ <pedropmancillac@gmail.com>
2022-04-01 16:38:48 -0300

    validacion formulario

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,27 @@
 // 
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                } else {
+                    alert('¡Mensaje enviado!')
+                }
+                form.classList.add('was-validated')
+            }, false)
+        })
+})()
+
+
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="collapse"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
@@ -10,10 +33,13 @@ const collapse = (icons) => {
     if ($(document).width() < 768) {
         icons.addClass('collapsed');
         $('.parrafos').removeClass('show');
+        return;
     }
+    icons.removeClass('collapsed');
+    $('.parrafos').addClass('show');
 }
 
-const iconsTool = (icons) => icons.attr('data-bs-original-title', `${icons.hasClass('collapsed') ? 'Mostrar' : 'Ocultar' }`)
+const iconsTool = (icons) => icons.attr('data-bs-original-title', `${icons.hasClass('collapsed') ? 'Mostrar' : 'Ocultar'}`)
 
 $(function () {
     $(document).scroll(function () { $('nav').attr('style', `background-color: rgba(0,0,0,${window.scrollY / window.innerHeight})`); });
@@ -23,4 +49,5 @@ $(function () {
 
     iconsTool(icons);
     $(`i[data-bs-toggle="collapse"]`).hover(function () { iconsTool($(this)) });
+
 });
