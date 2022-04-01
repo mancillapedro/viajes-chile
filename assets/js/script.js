0cea7b0a5e9fff2aeade3c33fad55588c6010941
_ <pedropmancillac@gmail.com>
2022-04-01 20:11:20 -0300

    columna del medio reverse

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,32 +21,39 @@
         })
 })()
 
-
+// habilitar tooltip pero en collapse
 var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="collapse"]'))
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
-const icons = $(`i[data-bs-toggle="collapse"]`);
-
-const collapse = (icons) => {
-    if ($(document).width() < 768) {
-        icons.addClass('collapsed');
-        $('.parrafos').removeClass('show');
-        return;
-    }
-    icons.removeClass('collapsed');
-    $('.parrafos').addClass('show');
-}
-
-const iconsTool = (icons) => icons.attr('data-bs-original-title', `${icons.hasClass('collapsed') ? 'Mostrar' : 'Ocultar'}`)
-
 $(function () {
+    const icons = $(`i[data-bs-toggle="collapse"]`);
+
+    const iconsTool = (icons) => icons.attr('data-bs-original-title', `${icons.hasClass('collapsed') ? 'Mostrar' : 'Ocultar'}`)
+
+    const collapse = (icons) => {
+        const parrafos = $('.parrafos');
+        const colCenter = $('#col-center')
+        if ($(document).width() < 768) {
+            icons.addClass('collapsed');
+            parrafos.removeClass('show ');
+            colCenter.removeClass('d-flex flex-column-reverse');
+            return;
+        }
+        icons.removeClass('collapsed');
+        parrafos.addClass('show');
+        colCenter.addClass('d-flex flex-column-reverse');
+    }
+
+    // navbar efecto
     $(document).scroll(function () { $('nav').attr('style', `background-color: rgba(0,0,0,${window.scrollY / window.innerHeight})`); });
 
+    // ocultar/mostrar parrafos
     collapse(icons);
     $(window).resize(function () { collapse(icons) });
 
+    // cambio de title de tooltip
     iconsTool(icons);
     $(`i[data-bs-toggle="collapse"]`).hover(function () { iconsTool($(this)) });
 
